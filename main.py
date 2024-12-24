f2e86cfeb0db1ca2679d3150aca297152c3d6fd1
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2024-12-25 03:24:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,3 +16,11 @@ def add_task(tasks):
     tasks.append(task)
     print("Task added successfully!")
 
+def view_tasks(tasks):
+    if tasks:
+        print("\nYour Tasks:")
+        for idx, task in enumerate(tasks, start=1):
+            print(f"{idx}. {task}")
+    else:
+        print("\nNo tasks available. Add some!")
+
